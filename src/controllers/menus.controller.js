7ef64c7691b7d8da908c615fa0e544fd8e04d727
Jorge Gramajo <Jorge Gramajo>
2024-07-01 22:23:47 -0300

    Se actualizo

--- src/controllers/menus.controller.js
+++ src/controllers/menus.controller.js
@@ -2,34 +2,7 @@ import db from "../database/connection.js";
 //const db = myConnection;
 
 
-export const getAllMenus = async (req, res) => {
-    const sql = 'SELECT * FROM cocina_italiana.Menus;';
-    try {
-        const [result] = await db.promise().query(sql);
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al obtener los menús' });
-    }
-
-}
-
-
-export const getMenuById = async (req, res) => {
-    const {id} = req.params;
-    const sql = `SELECT * FROM cocina_italiana.Menus WHERE Menus.id = ?;`;
-    try {
-        const [result] = await db.promise().query(sql, [id]);
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al obtener los menús' });
-    }
-
-}
-
-/*
-menuController.getAllMenus = (req, res) => {
+export const getAllMenus = (req, res) => {
     const sql = 'SELECT * FROM cocina_italiana.Menus;';
 
     db.query(sql, (err, result)=> {
@@ -40,4 +13,4 @@ menuController.getAllMenus = (req, res) => {
 
 }
 
-*/
+
